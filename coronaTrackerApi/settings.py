f0d0c70821a3834e9609cf6c98cf9d1dc0d1607c
tumzied <abdur963rahman@gmil.com>
2020-04-14 23:28:12 +0600

    debug false

--- coronaTrackerApi/settings.py
+++ coronaTrackerApi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(5w1zw=l*qs0=huw*)v%*1he@j8(!%ei(@mui8c_s(0pu+=c87'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
